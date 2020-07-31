ab64956526b8f04882668f58071f5073fff367e2
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-07-30 23:21:18 -0700

    Error Message Edit and Home Button
    Erroe Message pops up with different font than number.
    
    Home Button allows for navigation back to homescreen from chosen page.

--- randNumGen.js
+++ randNumGen.js
@@ -16,20 +16,38 @@ function main() {
     var minNum = document.getElementById("minNum").value.toString(); 
     var maxNum = document.getElementById("maxNum").value.toString(); 
 
-    if(isNaN(minNum) || minNum.indexOf(".") != -1) 
-        document.getElementById("randNumResult").innerHTML = "Minimum must be an INTEGER"; 
+    //If an invalid interval is given, an error message is displayed
+    //If a valid interval is given, a random number is displayed 
+    if(isNaN(minNum) || minNum.indexOf(".") != -1) {
+        document.getElementById("errorMessage").innerHTML = "Minimum must be an INTEGER"; 
 
-    else if(isNaN(maxNum) || maxNum.indexOf(".") != -1) 
-        document.getElementById("randNumResult").innerHTML = "Maximum must be an INTEGER"; 
+        document.getElementById("randNumResult").style.display = "none";
+        document.getElementById("errorMessage").style.display = "inline"; 
+    }
+
+    else if(isNaN(maxNum) || maxNum.indexOf(".") != -1) {
+        document.getElementById("errorMessage").innerHTML = "Maximum must be an INTEGER"; 
+
+        document.getElementById("randNumResult").style.display = "none";
+        document.getElementById("errorMessage").style.display = "inline";
+    }
    
-    else if (parseInt(minNum) > parseInt(maxNum)) 
-        document.getElementById("randNumResult").innerHTML = "Minimum must be LESS than the Maximum"; 
+    else if (parseInt(minNum) > parseInt(maxNum)) {
+        document.getElementById("errorMessage").innerHTML = "Minimum must be LESS than the Maximum"; 
+
+        document.getElementById("randNumResult").style.display = "none";
+        document.getElementById("errorMessage").style.display = "inline";
+    }
     
     else {
         minNum = parseInt(minNum); 
         maxNum = parseInt(maxNum); 
 
         var randNum = randNumGenerator(minNum, maxNum).toString(); 
+
+
+        document.getElementById("errorMessage").style.display = "none";
+        document.getElementById("randNumResult").style.display = "inline";
 
         document.getElementById("randNumResult").innerHTML = randNum;
     } 
